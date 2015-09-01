467aa37d2070c0d305328935eedb40994497f3e1
rdhutto <rdhutto1@gmail.com>
2015-09-01 13:05:46 -0400

    Added entrypoint.sh, added README.md, updated Dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # centos-zookeeper
+
+ZooKeeper
+
+https://zookeeper.apache.org/
+
+[![](https://badge.imagelayers.io/roberthutto/centos-zookeeper.svg)](https://imagelayers.io/?images=roberthutto/centos-zookeeper)
+
+Example
+
+docker run --name some-zookeeper \
+-e MY_ID=1 -e ENSEMBLE_HOST_NAMES=Node1,Node2 \
+-p 2181:2181 -p 2888:2888 -p 3888
+-d roberthutto/centos-zookeeper
---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# The myid file consists of a single line containing only the text of that machine's id.
+# So myid of server 1 would contain the text "1" and nothing else. The id must be unique within the
+# ensemble and should have a value between 1 and 255.
+echo ${MY_ID:-1} > /data/zookeeper/myid
+
+#Build the zookeeper config
+cat > /opt/zookeeper/conf/zoo.cfg <<EOF
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/data/zookeeper
+clientPort=2181
+EOF
+
+# server.1=...
+if [ -n "$ENSEMBLE_HOST_NAMES" ]; then
+    python -c "print '\n'.join(['server.%i=%s:2888:3888' % (i + 1, x) for i, x in enumerate('$ENSEMBLE_HOST_NAMES'.split(','))])" >> /opt/zookeeper/conf/zoo.cfg
+fi
+
+exec "$@"
